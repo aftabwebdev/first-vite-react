b666d8d5a50b7cfdd4a995a215dba4ecda4ed8e1
Aftab <aftab2060@gmail.com>
2023-11-04 19:47:34 +0600

    modified

--- src/pages/PracticeList.jsx
+++ src/pages/PracticeList.jsx
@@ -2,25 +2,30 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const PracticeList = () => {
 	return (
-		<div className="practice-list">
-			<h1>Practice List</h1>
-			<nav>
-				<ul>
-					<li>
-						<NavLink to="/practice-list/counter">Counter</NavLink>
-					</li>
-					<li>
-						<NavLink to="/practice-list/use-state-object">
-							Use State Object
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/practice-list/otherlink">Others</NavLink>
-					</li>
-				</ul>
-			</nav>
+		<>
+			<div className="practice-list">
+				<span className="menu">Menu</span>
+				<nav>
+					<ul>
+						<li>
+							<NavLink to="/practice-list/counter">Counter</NavLink>
+						</li>
+						<li>
+							<NavLink to="/practice-list/use-state-object">
+								Use State Object
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/practice-list/otherlink">Others</NavLink>
+						</li>
+						<li>
+							<NavLink to="/practice-list/products">Products</NavLink>
+						</li>
+					</ul>
+				</nav>
+			</div>
 			<Outlet />
-		</div>
+		</>
 	);
 };
 export default PracticeList;
